1a319635d006869a6fc0788e9567b4e8dc8845e6
varunGannavarapu <32113382+varunGannavarapu@users.noreply.github.com>
2019-06-02 20:14:39 -0400

    I am Rich
    Basic Application, familiarize user with how the XCode environment works

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Varun Gannavarapu on 5/30/19.
+//  Copyright © 2019 Varun Gannavarapu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
